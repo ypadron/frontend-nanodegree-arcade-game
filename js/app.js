8624ba271271af2b4456465d5fdaed581292ce93
ypadron <ypadron82@gmail.com>
2018-07-29 15:15:39 -0400

    feat: Add Player class update & render methods

--- js/app.js
+++ js/app.js
@@ -27,7 +27,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const player = function() {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = "images/char-cat-girl.png";
+};
 
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
