7d7c7fccbdf825803ed70660bf5d76ee3ef3c307
ypadron <ypadron82@gmail.com>
2018-07-30 14:59:52 -0400

    feat: Add Player method

--- js/app.js
+++ js/app.js
@@ -47,6 +47,11 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//This method receives user input via allowedKeys. Moves player around the board.
+Player.prototype.handleInput = function(dt) {
+
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
