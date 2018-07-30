4d1dcf8b8ee7285d087fc062f3231e31026adefa
ypadron <ypadron82@gmail.com>
2018-07-30 14:39:24 -0400

    feat: Modify Player constructor function

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const player = function() {
+const Player = function() {
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -50,8 +50,6 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
