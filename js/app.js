2d104c445cebb729a804f632b831290f87baaabb
ypadron <ypadron82@gmail.com>
2018-07-31 10:19:00 -0400

    feat: Push bugOne into enemy array

--- js/app.js
+++ js/app.js
@@ -54,9 +54,11 @@ Player.prototype.handleInput = function(dt) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const bugOne = new Enemy();
+
 const allEnemies = [];
 
-const bugOne = new Enemy();
+allEnemies.push(bugOne);
 
 const player = new Player();
 
