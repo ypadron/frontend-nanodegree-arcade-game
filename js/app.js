08ac24ca793e2924b858529a93225723a708e464
ypadron <ypadron82@gmail.com>
2018-08-09 22:23:00 -0400

    fix: Modify Player object and methods

--- js/app.js
+++ js/app.js
@@ -41,12 +41,16 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 const Player = function() {
     this.x = 200;
-    this.y = 400;
+    this.y = 395;
+    // this.startX = 200;
+    // this.startY = 400;
     this.sprite = "images/char-cat-girl.png";
 };
 
@@ -56,6 +60,20 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //Check for Player/Enemy collision
+    for (let enemy of allEnemies) {
+
+        if (this.y === enemy.y) {
+          console.log("same row!!")
+            // alert("I'm hit. Back to Zero!");
+            // this.reset();
+      }
+      console.log(this.y, enemy.y);
+    }
+    //Check to see if player won/reached the water
+    if(this.y < 25) {
+      alert("You're Safe!!");
+    }
 };
 
 // Draw the player on the screen, required method for game
@@ -64,26 +82,27 @@ Player.prototype.render = function() {
 };
 
 //This method receives user input via allowedKeys. Moves player around the board.
+//x (101) & y (83) movement = to column and row grid dimensions from engine.js
 Player.prototype.handleInput = function(direction) {
     switch (direction) {
       case "up":
-        if (this.y > 0) {
-          this.y -= 95;
+        if (this.y > -101) {
+          this.y -= 83;
       }
       break;
       case "down":
-        if (this.y < 400) {
-          this.y += 95;
+        if (this.y < 395) {
+          this.y += 83;
         }
         break;
       case "right":
         if (this.x < 350) {
-          this.x += 95;
+          this.x += 101;
         }
         break;
       case "left":
         if (this.x > 10) {
-          this.x -= 95;
+          this.x -= 101;
         }
         break;
       }
